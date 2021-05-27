65e59b53069a3fa6eb75c9fb33efc8da62036d58
Hassan <hassanalsabeh4@gmail.com>
2021-05-27 21:38:00 +0300

    Step_5 completed

---
+++ javascript-basics-01/Step_5.js
@@ -0,0 +1,8 @@
+function doMath() {
+      var numOne = document.getElementById('first_number').value;
+      var numTwo = document.getElementById('second_number').value;
+      var theProduct = parseInt(numOne) * parseInt(numTwo);
+    
+      alert("The Multiplication : " + theProduct);
+     
+}
