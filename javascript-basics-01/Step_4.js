180908c93cef5810b4853517c934864d28f5c7d4
Hassan <hassanalsabeh4@gmail.com>
2021-05-27 20:45:18 +0300

    Step_4 completed

---
+++ javascript-basics-01/Step_4.js
@@ -0,0 +1,10 @@
+ function myFunction(){
+    var newLine = "\r\n"
+            var msg = "What is your name"
+            msg += newLine;
+            msg += "What is your surname";
+            msg += newLine;
+            msg += "What is your City";
+         
+            alert(msg);
+         }
