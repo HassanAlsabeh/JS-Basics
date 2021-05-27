492aa1228bc1e2eb5edba05ca354cbe5963ff6ba
Hassan <hassanalsabeh4@gmail.com>
2021-05-27 15:14:01 +0300

    step_2 completed

---
+++ javascript-basics-01/step_2.js
@@ -0,0 +1,11 @@
+
+function myFunction() {
+    var name = "Hassan ";
+var surname = "Alsabeh ";
+var city = "Beirut ";
+    document.getElementById("demo").innerHTML =
+    "Name : "+ name +"<br>"+
+    "SurName : "+surname +"<br>"+
+    "City : "+city;
+  
+}
