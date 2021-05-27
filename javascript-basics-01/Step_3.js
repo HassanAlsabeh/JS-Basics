168d5914ef445620a65bfab36737b55233093a27
Hassan <hassanalsabeh4@gmail.com>
2021-05-27 15:28:43 +0300

    Step_3 completed

---
+++ javascript-basics-01/Step_3.js
@@ -0,0 +1,9 @@
+
+         
+            function myFunction() {
+  var person = prompt("What is your name");
+  if (person != null) {
+    document.getElementById("demo").innerHTML =
+    "Hello " + person + "! How are you today?";
+  }
+}
