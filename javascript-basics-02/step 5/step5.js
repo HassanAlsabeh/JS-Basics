43bb961413fdb9e6e9d9d7cf23b30a6e98e4e3c3
Hassan <hassanalsabeh4@gmail.com>
2021-05-29 19:21:01 +0300

    Step 5 Completed

---
+++ javascript-basics-02/step 5/step5.js
@@ -0,0 +1,18 @@
+ function myFunction() {
+     var img = $("#img_home");
+     if (img.src = "images/image1.jpg"){
+         document.getElementById("image1").src="../step 5/images/image1_2.jpg"
+     }
+     else if (img.src = "images/image2.jpg"){
+         document.getElementById("image1").src="../step 5/images/image2_2.jpg"
+     }
+     else if (img.src = "images/image3.jpg"){
+         document.getElementById("image1").src="../step 5/images/image3_2.jpg"
+     }
+    else if (img.src = "images/image4.jpg"){
+         document.getElementById("image1").src="../step 5/images/image4_2.jpg"
+     }
+     else if (img.src = "images/image5.jpg"){
+         document.getElementById("image1").src="../step 5/images/image5_2.jpg"
+     }
+    }
