96e6bb5e2ab763fc4fee1bf1c041c68db16009de
Hassan <hassanalsabeh4@gmail.com>
2021-05-30 00:45:27 +0300

    Step 6 Completed

--- javascript-basics-02/step 6/step6.js
+++ javascript-basics-02/step 6/step6.js
@@ -1,7 +1,37 @@
 function myFunction() {
-     document.getElementById("image1").src="../step 5/images/image1_2.jpg"
-     document.getElementById("image2").src="../step 5/images/image2_2.jpg"
-     document.getElementById("image3").src="../step 5/images/image3_2.jpg"
-     document.getElementById("image4").src="../step 5/images/image4_2.jpg"
-     document.getElementById("image5").src="../step 5/images/image5_2.jpg"
+     
+     if (img.src = "images/image1.jpg"){
+         document.getElementById("image1").src="../step 6/images/image1_2.jpg"
+     }
+     else if (img.src = "images/image2.jpg"){
+         document.getElementById("image2").src="../step 6/images/image2_2.jpg"
+     }
+     else if (img.src = "images/image3.jpg"){
+         document.getElementById("image3").src="../step 6/images/image3_2.jpg"
+     }
+    else if (img.src = "images/image4.jpg"){
+         document.getElementById("image4").src="../step 6/images/image4_2.jpg"
+     }
+     else if (img.src = "images/image5.jpg"){
+         document.getElementById("image5").src="../step 6/images/image5_2.jpg   "
+     }
+    }
+
+    function myFunctionSec() {
+     
+     if (img.src = "images/image1_2.jpg"){
+         document.getElementById("image1").src="../step 6/images/image1.jpg"
+     }
+     else if (img.src = "images/image2_2.jpg"){
+         document.getElementById("image2").src="../step 6/images/image2.jpg"
+     }
+     else if (img.src = "images/image3_2.jpg"){
+         document.getElementById("image3").src="../step 6/images/image3.jpg"
+     }
+    else if (img.src = "images/image4_2.jpg"){
+         document.getElementById("image4").src="../step 6/images/image4.jpg"
+     }
+     else if (img.src = "images/image5_2.jpg"){
+         document.getElementById("image5").src="../step 6/images/image5.jpg   "
+     }
     }
