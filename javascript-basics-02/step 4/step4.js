d2b5aeef3aa3d86131104aea70f7998779c8a8b1
Hassan <hassanalsabeh4@gmail.com>
2021-05-29 18:37:36 +0300

    Step 4 Completed

---
+++ javascript-basics-02/step 4/step4.js
@@ -0,0 +1,15 @@
+
+      
+        function myFunction() {
+          var answer = window.confirm("Clear all data?");
+          if (answer) {
+            //some code
+            document.getElementById("name").value = '';
+            document.getElementById("surname").value = '';
+            document.getElementById("city").value = '';
+            
+          }
+         
+         
+  }
+  
