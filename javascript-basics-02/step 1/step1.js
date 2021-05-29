da279a6bb27d48c7a3d882d900e41d51e02f61dd
Hassan <hassanalsabeh4@gmail.com>
2021-05-29 18:31:20 +0300

    Step 1 Completed

---
+++ javascript-basics-02/step 1/step1.js
@@ -0,0 +1,7 @@
+
+       function myFunction() {
+         document.getElementById("image1").src="../step 1/images/image1_2.jpg"
+       }
+         function myFunction2() {
+            document.getElementById("image1").src = "../step 1/images/image1.jpg"
+          }
